a308d605aa95b15b5f0cf924799c3599e98cc565
Mauricio F Martinez <mauriciof.martinez@gmail.com>
2016-12-09 20:06:03 -0800

    added the products table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161209103725) do
+ActiveRecord::Schema.define(version: 20161210035550) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -41,6 +41,18 @@ ActiveRecord::Schema.define(version: 20161209103725) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string   "title"
+    t.string   "subtitle"
+    t.string   "image_name"
+    t.string   "sku"
+    t.string   "details"
+    t.text     "description"
+    t.decimal  "price"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "purchases", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,5 +9,10 @@ AdminUser.create!(email: 'admin@example.com', password: 'password', password_con
 
 sbGold = Product.create(title: "Santa Barbara Gold",
 subtitle: "Santa Barbara Gold", image_name: "SB Gold.jpg"
-price: "9.99", sku: "sbGold", details: "bright gold, yellow or orange color; firm buttery texture; fresh salty ocean scent; and with a sweet buttery taste.  Uni sections are large and complete intact pieces.  This is exceptionally high-grade uni for use in top quality sushi. (formerly grade A)", description: %{<p>Santa Barbara Gold</p>
+price: "9.99", sku: "sbGold", details: "Bright gold, yellow or orange color; firm buttery texture; fresh salty ocean scent; and with a sweet buttery taste.  Uni sections are large and complete intact pieces.  This is exceptionally high-grade uni for use in top quality sushi. (formerly grade A)", description: %{<p>Santa Barbara Gold</p>
+})
+
+sbPremium = Product.create(title: "Santa Barbara Premium",
+subtitle: "Santa Barbara Premium", image_name: "SB Premium.jpg"
+price: "8.99", sku: "sbPremium", details: "Gold, yellow or orange color but less brilliant than California Gold; firm buttery texture; salty ocean scent; with a crisp and nutty taste.  Uni sections are smaller but still primarily intact pieces.  Premium uni is used for sushi, soups, salads, or combination dishes where uni is the featured item.", description: %{<p>Santa Barbara Premium</p>
 })
