590615ffb0aadcab8f41069b9d965a18002d989f
Mauricio F Martinez <mauriciof.martinez@gmail.com>
2016-12-09 19:50:15 -0800

    added first product to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+sbGold = Product.create(title: "Santa Barbara Gold",
+subtitle: "Santa Barbara Gold", image_name: "SB Gold.jpg"
+price: "9.99", sku: "sbGold", details: "bright gold, yellow or orange color; firm buttery texture; fresh salty ocean scent; and with a sweet buttery taste.  Uni sections are large and complete intact pieces.  This is exceptionally high-grade uni for use in top quality sushi. (formerly grade A)", description: %{<p>Santa Barbara Gold</p>
+})
